35a33f9bc6b3127e1f1e8046a66b336590348f73
unknown <usern5949@gmail.com>
2024-02-17 14:42:54 +0530

    Added video card and channel card

--- yt-clone/src/Components/Videos.jsx
+++ yt-clone/src/Components/Videos.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import { Stack, Box } from "@mui/material";
+import VideoCard from "./VideoCard";
+import ChannelCard from "./ChannelCard";
 
 const Videos = ({ videos }) => {
   //   console.log(videos);
-  return;
+  return (
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+      {videos.map((item, i) => (
+        <Box key={i}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
+  );
 };
 
 export default Videos;
